152796419c716d449c6b87edc7e30b0b91c40cc2
hoheps <38477462+hoheps@users.noreply.github.com>
2019-02-25 10:56:39 -0500

    changed pins and added keystroke

--- KappaPad/CapacitiveKey.h
+++ KappaPad/CapacitiveKey.h
@@ -6,15 +6,15 @@ class CapacitiveKey {
     unsigned int releaseDelay = 20;
     unsigned int releaseTimer;
     unsigned int treshold;  
-    int led;
+    //int led;
     unsigned int sample;
-    unsigned char ledBrightness;
+    //unsigned char ledBrightness;
     CapacitiveKey(uint8_t sendPin, uint8_t receivePin, int statusLED, unsigned int capacitiveTreshold, char keyboardKey, unsigned char ledBrightness)  {
       sensor = new CapacitiveSensor(sendPin, receivePin);
       treshold = capacitiveTreshold;
       key = keyboardKey;
-      led = statusLED;
-      pinMode(led, OUTPUT);
+      //led = statusLED;
+      //pinMode(led, OUTPUT);
     }
     ~CapacitiveKey() {
       delete sensor;
@@ -23,7 +23,7 @@ class CapacitiveKey {
       sample = sensor->capacitiveSensorRaw(1);
       if (sample > treshold) {
         if (keyReleased) {
-          analogWrite(led, 255);
+          //analogWrite(led, 255);
           if (keyboardActive) Keyboard.press(key);
           keyReleased = false;
         }
@@ -32,7 +32,7 @@ class CapacitiveKey {
       else {
         if (!keyReleased) {
           if (releaseTimer == 0) {
-            analogWrite(led, 0);
+            //analogWrite(led, 0);
             Keyboard.release(key);
             keyReleased = true;
           }
--- KappaPad/KappaPad.ino
+++ KappaPad/KappaPad.ino
@@ -3,7 +3,7 @@
 #include <CapacitiveSensor.h>
 #include <Keyboard.h>
 
-#include "capacitiveKey.h"
+#include "CapacitiveKey.h"
 
 //#define SERIAL_OUTPUT
 #define DISABLE_PIN 15
@@ -18,25 +18,33 @@ void setup() {
 
 CapacitiveKey key0 = CapacitiveKey(
   2,    //Capacitive Send Pin
-  7,    //Capacitive Sense Pin
+  8,    //Capacitive Sense Pin
   6,    //LED Pin
   6,    //Capacitive Treshold
-  'z',  //Keyboard Key
+  KEY_ESC,  //Keyboard Key
   255   //LED Brightness (0-255)
 );
 CapacitiveKey key1 = CapacitiveKey(
-  4,    //Capacitive Send Pin
-  8,    //Capacitive Sense Pin
+  7,    //Capacitive Send Pin
+  4,    //Capacitive Sense Pin
   10,   //LED Pin
   5,    //Capacitive Treshold
   'x',  //Keyboard Key
   255   //LED Brightness (0-255)
 );
-
+CapacitiveKey key2 = CapacitiveKey(
+  18,    //Capacitive Send Pin
+  21,    //Capacitive Sense Pin
+  10,   //LED Pin
+  5,    //Capacitive Treshold
+  'z',  //Keyboard Key
+  255   //LED Brightness (0-255)
+);
 void loop() {
   bool keyboardActive = digitalRead(DISABLE_PIN);
   key0.keyUpdate(keyboardActive);
   key1.keyUpdate(keyboardActive);
+  key2.keyUpdate(keyboardActive);
 
   #ifdef SERIAL_OUTPUT
   Serial.print(key0.sample);
@@ -44,5 +52,3 @@ void loop() {
   Serial.println(key1.sample);
   #endif
 }
-
-
